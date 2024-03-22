cf8360a7c0f8858b467c16cc66624b40883edd6e
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-03-21 21:31:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import axios from "axios"
+import {useEffect,useState} from "react"
 function App() {
+  const [text,setText]=useState("")
+  console.log(process.env.REACT_APP_BACKENDURL)
+  useEffect(()=>{
+    axios.get(`${process.env.REACT_APP_BACKENDURL}`)
+  .then( (response)=> {
+    console.log(response);
+    setText(response.data)
+  })
+  .catch( (error) =>{
+    // handle error
+    console.log(error);
+  })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{text}</p>
     </div>
   );
 }
